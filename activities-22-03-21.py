d28f76fa63c812c6d4f55345e06f443c307a621a
hash-luk <lucasgamerhd18@gmail.com>
2022-03-22 15:11:59 -0300

    Finished Exercises

--- activities-22-03-21.py
+++ activities-22-03-21.py
@@ -1,12 +1,5 @@
-
-#1 - Escreva um algoritmo que imprima na tela: Olá Mundo!
-from cgitb import reset
-from dis import dis
 import math
-import numbers
-from unittest import result
-
-
+#1 - Escreva um algoritmo que imprima na tela: Olá Mundo!
 def printHelloWorld():
     print("Hello World")
 
@@ -319,14 +312,302 @@ def convertMetrics():
 
     print(f"A conversão para polegadas resulta em: {pol}\nA conversão para jardas resulta em: {jar}\nA conversão para milhas resilta em: {mil}")
 
-#Escreva um programa que receba como entrada um número de 5 dígitos, separe o número em dígitos individuais e os imprima separados por 3 espaços cada um. Por exemplo, se o usuário digitar 42339, o programa deverá imprimir: 4 2 3 3. Dica: utilize as operações de divisão e módulo para extrair cada dígito do número.
+#26 - Escreva um programa que receba como entrada um número de 5 dígitos, separe o número em dígitos individuais e os imprima separados por 3 espaços cada um. Por exemplo, se o usuário digitar 42339, o programa deverá imprimir: 4 2 3 3. Dica: utilize as operações de divisão e módulo para extrair cada dígito do número.
 def print5DigitsNumber():
     number = ""
+    formatedNumber = []
 
     while len(number) < 5:
         number = input("Digite um número com 5 digitos (Apenas números): ")
 
         if len(number) >= 5:
             for i in range(len(number)):
-                print(number[i])
-print5DigitsNumber()
+                formatedNumber.append(str(number[i]))
+                formatedNumber.append(str([]))
+                formatedNumber.append(str([]))
+                formatedNumber.append(str([]))
+            print("".join(formatedNumber).replace("[]"," "))
+            
+#27 - A nota final de um estudante é calculada a partir de três notas atribuídas, respectivamente, a um trabalho de laboratório, a uma avaliação semestral e a um exame final. A média das três notas mencionadas obedece aos pesos a seguir:
+#Faça um programa que receba as três notas, calcule e mostre a média ponderada e o conceito que segue a tabela
+def gradeMedia():
+    laboratoryProjectP = 2
+    semiannualExameP = 3
+    finalExameP = 5
+    sommatoryP = laboratoryProjectP + semiannualExameP + finalExameP
+    ponMedia = 0
+
+    laboratoryProject = float(input("Insira a nota do trabalho de laboratório (Apenas números): "))
+    semiannualExame = float(input("Insira a nota da avaliação semestral (Apenas números): "))
+    finalExame = float(input("Insira a nota do exame final (Apenas números): "))
+
+    ponMedia = (laboratoryProjectP * laboratoryProject + semiannualExameP * semiannualExame + finalExameP * finalExame) / sommatoryP
+
+    if ponMedia >= 8 and ponMedia <= 10:
+        print(f"A média foi: {ponMedia}. O conceito recebido foi A.")
+    elif ponMedia >= 7 and ponMedia < 8:
+        print(f"A média foi: {ponMedia}. O conceito recebido foi B.")
+    elif ponMedia>= 6 and ponMedia < 7:
+        print(f"A média foi: {ponMedia}. O conceito recebido foi C.")
+    elif ponMedia>=5 and ponMedia < 6:
+        print(f"A média foi: {ponMedia}. O conceito recebido foi D.")
+    elif ponMedia >= 0 and ponMedia< 5:
+        print(f"A média foi: {ponMedia}. O conceito recebido foi E.")
+    else:
+        print(f"A média foi {ponMedia}. Isso está acima dos casos de conceito")
+
+
+
+#28 - Faça um programa que receba três notas de um aluno, calcule e mostre a média aritmética e a mensagem constante na tabela a seguir.
+def studentMedia():
+    count = 0 
+    sommatory = 0 
+    media = 0
+    grades = []
+
+    while count < 3:
+        grades.append(float(input("Escreva uma nota (Apenas números): ")))
+        count += 1
+
+    for g in grades:
+        sommatory += g
+    
+    media = sommatory / len(grades)
+
+    if media >= 7 and media <= 10:
+        print(f"Aluno aprovado. A média foi {media:.2}")
+    elif media >= 3 and media < 7:
+        print(f"O aluno está em  Exme Especial. A média foi {media:.2f}")
+    elif media >= 0 and media < 3:
+        print(f"O aulo está reprovado. A média foi {media:.2f}")
+    else:
+        print(f"Média fora dos casos definidos. A média foi {media:.2f}")
+
+
+#29 - Faça um programa para resolver equações do 2º grau.
+
+def equation():
+    a = int(input("Defina o valor de A (Apenas números e diferente de 0): "))
+    b = int(input("Defina o valor de B (Apenas números): "))
+    c = int(input("Defina o valor de C (Apenas números): "))
+
+    if a != 0:
+        D = (b**2 - 4*a*c)
+        x1 = (-b + D**(1/2)) / (2*a)
+        x2 = (-b - D**(1/2)) / (2*a)
+
+        if isinstance(x1, int) or isinstance(x1,float) or isinstance(x1, int) or isinstance(x2,float):
+            print(f"O valor de x1 é: {x1}\nO valor de x2 é: {x2}")
+        else:
+            print("Os núemros informados não possuem raízes reais")
+    else:
+        print("A não pode ser igual a 0")
+
+#30 - Faça um programa que receba vários números, calcule e mostre:
+def calcVariousNumbers():
+    numbers = []
+    sommatory = 0 
+    media = 0
+    pares = []
+    impares = []
+    perCentImpar = 0
+    somaPar = 0
+    mediaPar = 0
+
+    while True:
+        numbers.append(float(input("Digite um número (Apenas números): ")))
+        userOption = int(input("Deseja adicionar mais números? Digite 0 para fechar ou 1 para continuar: "))
+
+        if(userOption == 0):
+            #A soma dos números digitados;
+            for n in numbers:
+                sommatory += n
+
+            print(f"A soma dos números é: {sommatory}")
+            #A quantidade de números digitados;
+            print(f"Foram digitados: {len(numbers)} número(s)")
+
+            #A média dos números digitados;
+            for n in numbers:
+                media = sommatory/len(numbers)
+            
+            print(f"A média dos números foi: {media}")
+
+            #O maior número digitado;
+            print(f"O maior número digitado foi: {max(numbers)}")
+
+            #O menor número digitado;
+            print(f"O menor número digitado foi: {min(numbers)}")
+
+            #A média dos números pares;
+            for n in numbers:
+                if n % 2 == 0:
+                    pares.append(n)
+            
+            for p in pares:
+                somaPar += p 
+            
+            mediaPar = somaPar/len(pares)
+
+            print(f"A média dos números pares é: {mediaPar}")
+
+            #A porcentagem dos números ímpares entre todos os números digitados.
+            for n in numbers:
+                if n % 2 != 0:
+                    impares.append(n)
+            
+            perCentImpar = len(impares)/len(numbers) * 100
+
+            print(f"A porcentagem de números ímpares é de: {perCentImpar}%")
+
+            break
+
+        
+#31 - Foi realizada uma pesquisa sobre algumas características físicas dos habitantes de uma região. Foram coletados os seguintes dados de cada habitante: sexo, cor dos olhos (A — azuis; ou C — castanhos), cor dos cabelos (L — louros; P — pretos; ou C — castanhos) e idade. Implemente os seguintes métodos:
+
+#Teria que ser criado uma classe para agrupar essas funções
+
+def media_idades(person):
+    pFilter = []
+    ages = []
+    sommatory = 0
+    media = 0
+
+    for p in person:
+        if p[1] == 'C' and p[2] == 'P':
+            pFilter.append(p)
+
+
+    for n in pFilter:
+        ages.append(n[3])
+
+    for a in ages:
+        sommatory += a
+    
+    media = sommatory/len(pFilter)
+    print(f"A média das idades é {media}")
+
+
+def max_age(person):
+    ages = []
+
+    for p in person:
+        ages.append(p[3])
+
+    print(f"A maior idade é: {max(ages)}")
+
+def freq_person(person):
+    filterFem = []
+
+    for p in person:
+        if p[0] == 'F':
+            if p[1] == 'A':
+                if p[2] == 'L':
+                    if p[3] > 18 and p[3]<=35:
+                        filterFem.append(p)
+
+    print(f"A quantidade de pessoa com as características desejadas é: {len(filterFem)}")
+
+
+def read_data():
+    userChoose = int(input("Informe a quatidade de cadastros a serem inseridos (Apenas números:"))
+    counter = 0
+
+    person = [] * userChoose
+
+    sexo = []
+    eyes = []
+    hairColor = []
+    age = []
+
+    while counter < userChoose:
+        sx = input("Informe o sexo (F/M): ")
+        sexo.append(sx.upper())
+        ey = input("Informe a cor dos olhos (A- Azuis/C- Castanhos): ")
+        eyes.append(ey.upper())
+        hair = input("Informe a cor do cabelo (L- Loiro, P- Preto, C- Castanho): ")
+        hairColor.append(hair.upper())
+        a = int(input("informe a idade (Apenas números): "))
+        age.append(a)
+
+        counter += 1
+
+    
+    for i in range(userChoose):
+        person.append((sexo[i],eyes[i],hairColor[i],age[i]))
+
+    media_idades(person)
+    max_age(person)
+    freq_person(person)
+
+
+#32 - Faça o método analisa_numeros(numeros) que receba uma lista de números como parâmetro, calcule e mostre:
+class ex32:
+    numbers = [1,2,3,4]
+    def analisa_numeros(numbers):
+        sommatory = 0 
+        media = 0
+        pares = []
+        impares = []
+        perCentImpar = 0
+        somaPar = 0
+        mediaPar = 0
+
+        #A Soma dos números;
+        for n in numbers:
+            sommatory += n
+
+        print(f"A soma dos números é: {sommatory}")
+
+        #A quantidade de números digitados;
+        print(f"Foram digitados: {len(numbers)} número(s)")
+
+        #A média dos números digitados;
+        for n in numbers:
+            media = sommatory/len(numbers)  
+
+        print(f"A média dos números foi: {media}")
+
+        #O maior número digitado;
+        print(f"O maior número digitado foi: {max(numbers)}")
+
+        #O menor número digitado;
+        print(f"O menor número digitado foi: {min(numbers)}")
+
+        #A média dos números pares;
+        for n in numbers:
+            if n % 2 == 0:
+                pares.append(n)
+            
+            for p in pares:
+                somaPar += p 
+            
+        mediaPar = somaPar/len(pares)
+
+        print(f"A média dos números pares é: {mediaPar}")
+
+        #A porcentagem dos números ímpares entre todos os números digitados.
+        for n in numbers:
+            if n % 2 != 0:
+                impares.append(n)
+            
+        perCentImpar = len(impares)/len(numbers) * 100
+
+        print(f"A porcentagem de números ímpares é de: {perCentImpar}%")
+
+#33 - Uma empresa quer transmitir dados pelo telefone, mas está preocupada com a interceptação telefônica. Todos os seus dados são transmitidos como inteiros de quatro dígitos. Ela pediu para que você escreva um programa que criptografe seus dados, para que eles possam ser transmitidos com mais segurança. Implemente a função criptogra(numero) que receba como parâmetro um número inteiro de quatro dígitos e criptografe-o da seguinte forma:
+def criptografia(number):
+    convertedNumber = list(str(number))
+    digitsP7 = []
+    switchNumbers = [0] * 4
+
+    for c in convertedNumber:
+        digitsP7.append(int(c) + 7)
+
+    for c in digitsP7:
+        switchNumbers[0] = digitsP7[2]
+        switchNumbers[1] = digitsP7[3]
+        switchNumbers[2] = digitsP7[0]
+        switchNumbers[3] = digitsP7[1]
+
+    return switchNumbers
